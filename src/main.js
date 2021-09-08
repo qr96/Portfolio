e54bac4f1a6c50b20ec030473d13f8c1d8ed3ddc
qr96 <qr96@naver.com>
2021-09-08 14:18:23 +0900

    update

--- src/main.js
+++ src/main.js
@@ -8,3 +8,21 @@ VanillaTilt.init(document.querySelectorAll(".card"), {
 TwinkleStar.init(document.querySelector('.scene'))
 
 
+window.addEventListener('scroll', () => {
+    const page0 = document.querySelector('#page0')
+
+	let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
+	let windowHeight = window.innerHeight; // 스크린 창
+	let fullHeight = document.body.scrollHeight; //  margin 값은 포함 x
+
+    console.log(scrollLocation, windowHeight, fullHeight)
+
+    if(scrollLocation*2 > windowHeight) {
+        page0.style.animation = "fadein 1s forwards "
+
+    }
+    else {
+        page0.style.animation = "fadeout 1s forwards "
+    }
+})
+
